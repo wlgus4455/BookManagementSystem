fc1ca98cd58c32e876600d5dffefdb00c2796dc7
wlgus4455 <jihyeon4455@naver.com>
2021-03-12 00:08:04 +0900

    Amend 'Student Management System'to'Book Management System'.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add Book");
+		System.out.println("2. Delete Book");
+		System.out.println("3. Edit Book");
+		System.out.println("4. View Book");
 		System.out.println("5. Exit");
 	}
 
