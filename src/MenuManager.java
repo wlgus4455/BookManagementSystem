69396afc86802af442c5bc625004f54d600346f5
wlgus4455 <jihyeon4455@naver.com>
2021-03-30 03:00:56 +0900

    Change "Book Management System Menu" to "Clothes Management System Menu"

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -7,26 +7,26 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		
 		while (num !=6) {
-		System.out.println("< Book Management System Menu>");
-		System.out.println("1. Add Book");
-		System.out.println("2. Delete Book");
-		System.out.println("3. Edit Book");
-		System.out.println("4. View Book");
+		System.out.println("< Clothes Management System Menu>");
+		System.out.println("1. Add Clothes");
+		System.out.println("2. Delete Clothes");
+		System.out.println("3. Edit Clothes");
+		System.out.println("4. View Clothes");
 		System.out.println("5. Show a Menu");
 		System.out.println("6. Exit");
-		System.out.println("Select one number between 1-6:");
+		System.out.print("Select one number between 1-6:");
 		num=input.nextInt();
 		if(num==1) {
-			addbook();
+			addClothes();
 		}
 		else if (num==2) {
-			deleteBook();
+			deleteClothes();
 		}
 		else if (num==3) {
-			editBook();
+			editClothes();
 		}
 		else if (num==4) {
-			viewbook();
+			viewClothes();
 		}
 		else {
 			        continue;
@@ -35,34 +35,39 @@ public class MenuManager {
 
   }
 
-	public static void addbook() {
+	public static void addClothes() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Book Number:");
+		System.out.print("Clothes Number:");
 		int booknumber = input.nextInt();
-		System.out.print("Book Title:");
+		
+		System.out.print("Clothes Title:");
 		String booktitle = input.next();
-		System.out.print("Book Genre:");
+		
+		System.out.print("ClothesGenre:");
 		String bookgenre = input.next();
-		System.out.print("Book Writer:");
+		
+		System.out.print("Clothes Writer:");
 		String bookwriter = input.next();
-		System.out.print("Book Publisher:");
+		
+		System.out.print("Clothes Publisher:");
 		String bookpublisher = input.next();
+		
 		
 	}
 
-	public static void deleteBook() {
+	public static void deleteClothes() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Book Number:");
 		int booknumber = input.nextInt();		
 	}
 
-	public static void editBook() {
+	public static void editClothes() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Book Number:");
 		int booknumber = input.nextInt();		
 	}
 
-	public static void viewbook() {
+	public static void viewClothes() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Book Number:");
 		int booknumber = input.nextInt();		
